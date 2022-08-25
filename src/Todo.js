c1a97f573fe0580fedad2ae0bc8a5ba078703b2d
HoJun Kim <k8759102@gmail.com>
2022-08-25 23:56:35 +0900

    Todo 클래스 정의

---
+++ src/Todo.js
@@ -0,0 +1,10 @@
+class Todo {
+  /** @param {todoModel} data */
+  constructor(data) {}
+
+  /**
+   * @function
+   * @param {todoModel} data
+   */
+  initialize = function (data) {};
+}
---
+++ src/global.js
@@ -0,0 +1,9 @@
+/**
+ * @typedef todoModel
+ * @type {object}
+ * @property {number} id - 아이디
+ * @property {string} content - 내용
+ * @property {boolean} isOk - 완료여부
+ * @property {string} category - 카테고리
+ * @property {Array.<string>} [tags] - 태그들
+ */
